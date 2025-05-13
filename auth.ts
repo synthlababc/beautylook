a389b1c846902acfbf6c3f06ddb3bd09804a607d
synthlababc <synthlababc@gmail.com>
2025-05-14 02:53:39 +0800

    api

--- auth.ts
+++ auth.ts
@@ -1,11 +1,15 @@
-import NextAuth from 'next-auth';
+import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID!,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-        })
-      ],
+export const { 
+  handlers: { GET, POST }, 
+  auth 
+} = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET
 })

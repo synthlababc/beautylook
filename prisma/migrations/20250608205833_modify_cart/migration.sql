48271568c869217d7edae338d86ce46ee22cf701
synthlababc <synthlababc@gmail.com>
2025-06-10 04:16:58 +0800

    fix

--- prisma/migrations/20250608205833_modify_cart/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Cart" ADD COLUMN     "deletedAt" TIMESTAMP(3);
--- prisma/migrations/20250609201524_init/migration.sql
+++ prisma/migrations/20250609201524_init/migration.sql
@@ -109,6 +109,7 @@ CREATE TABLE "CartItem" (
     "quantity" INTEGER NOT NULL DEFAULT 1,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deletedAt" TIMESTAMP(3),
 
     CONSTRAINT "CartItem_pkey" PRIMARY KEY ("id")
 );

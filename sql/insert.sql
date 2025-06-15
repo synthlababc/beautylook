2afd51ca64d1f944a6a6b0b7e9849aa5b89c7e6e
synthlababc <synthlababc@gmail.com>
2025-06-15 15:53:17 +0800

    fix

--- sql/insert.sql
+++ sql/insert.sql
@@ -1,21 +1,21 @@
-INSERT INTO public."Product" ("name",description,price,"currency",status,image,detail,"createdAt","updatedAt","categoryId") VALUES
-	 ('4 Glov Micro Infusion System + 12 Serums + 8 Needles','Includes :
+INSERT INTO public."Product" (id, "name",description,price,"currency",status,image,detail,"createdAt","updatedAt","categoryId") VALUES
+	 (4,'4 Glov Micro Infusion System + 12 Serums + 8 Needles','Includes :
 (4) Glov Micro Infusion System
 (12) Glov Anti-Aging Serums
 (8) Sterile Needle Heads
 ',5600.000000000000000000000000000000,'USD'::public."Currency",'NORMAL'::public."ProductStatus",'/micro/4+12+8.png','','2025-06-15 03:20:11.652','2025-06-15 03:20:11.652',1),
-	 ('2 Glov Micro Infusion System Bundle + 6 Serums + 4 Needles','Includes :
+	 (3,'2 Glov Micro Infusion System Bundle + 6 Serums + 4 Needles','Includes :
 (2) Glov Micro Infusion System
 (6) Glov Anti-Aging Serums
 (4) Sterile Needle Heads',4600.000000000000000000000000000000,'USD'::public."Currency",'NORMAL'::public."ProductStatus",'/micro/2+6+4.png','','2025-06-15 03:05:22.912','2025-06-15 03:05:22.912',1),
-	 ('Glov Micro Infusion System Bundle + 3 Serums + 2 Needles','Includes :
+	 (2,'Glov Micro Infusion System Bundle + 3 Serums + 2 Needles','Includes :
 (1) Glov Micro Infusion System
 (3) Glov Anti-Aging Serums
 (2) Sterile Needle Heads
 ',3800.000000000000000000000000000000,'USD'::public."Currency",'NORMAL'::public."ProductStatus",'/micro/1+3+2.png','','2025-06-15 03:05:21.007','2025-06-15 03:05:21.007',1),
-	 ('Glov Anti-Aging Serum (5ml)','Our serum ampoule delivers long-lasting hydration, balancing the skin''s water and oil levels, reducing excess sebum, and keeping the skin deeply moisturized',500.000000000000000000000000000000,'USD'::public."Currency",'NORMAL'::public."ProductStatus",'/micro/anti.png','1','2025-06-15 03:05:21.007','2025-06-15 03:05:21.007',1),
-	 ('Sterile Needle Heads (2)','Refills include sterile needle heads to attach to your Micro-Infusion device',700.000000000000000000000000000000,'USD'::public."Currency",'NORMAL'::public."ProductStatus",'/micro/head.png','1','2025-06-15 03:05:22.912','2025-06-15 03:05:22.912',1),
-	 ('Glov Micro Infusion System','Enhances skin elasticity and reduces the appearance of fine lines and wrinkles while delivering active serum ingredients onto your skin
+	 (5,'Glov Anti-Aging Serum (5ml)','Our serum ampoule delivers long-lasting hydration, balancing the skin''s water and oil levels, reducing excess sebum, and keeping the skin deeply moisturized',500.000000000000000000000000000000,'USD'::public."Currency",'NORMAL'::public."ProductStatus",'/micro/anti.png','1','2025-06-15 03:05:21.007','2025-06-15 03:05:21.007',1),
+	 (6,'Sterile Needle Heads (2)','Refills include sterile needle heads to attach to your Micro-Infusion device',700.000000000000000000000000000000,'USD'::public."Currency",'NORMAL'::public."ProductStatus",'/micro/head.png','1','2025-06-15 03:05:22.912','2025-06-15 03:05:22.912',1),
+	 (1,'Glov Micro Infusion System','Enhances skin elasticity and reduces the appearance of fine lines and wrinkles while delivering active serum ingredients onto your skin
 
 ✔ Smooths Wrinkles And Fine Lines
 ✔ Stimulates Collagen Production
